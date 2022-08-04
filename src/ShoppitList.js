2d4e2982ec587c27eb490b70c157920ed6d31f3b
Phaedon Valasiadis <faival@protonmail.com>
2022-08-04 14:47:19 +0200

    added shoppitList & imports

---
+++ src/ShoppitList.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { Shoppit } from './Shoppit'
+import { SwipeListView } from 'react-native-swipe-list-view'
+import { VStack, Input, Icon, NativeBaseProvider, Box, Heading } from 'native-base'
+import uuid from 'uuid-random'
+import { FontAwesome5 } from '@expo/vector-icons'
+
+const ShoppitList = () => {
+  //receive state & dispatch from App.js
+
+}
+
+export default ShoppitList
